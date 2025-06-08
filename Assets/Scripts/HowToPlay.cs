b44a1857bb4c7ea9af21e2459e8d3b587f18a6de
Trale <trale@gmail.com>
2025-06-08 17:00:48 +0200

    pocetni ekran

---
+++ Assets/Scripts/HowToPlay.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class HowToPlay : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+}
